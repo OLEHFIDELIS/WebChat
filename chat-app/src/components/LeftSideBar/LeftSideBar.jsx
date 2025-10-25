fd340b156e31fcbd92a62d00dad2b924bc319284
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-10-25 13:35:36 +0100

    worked on the right side ui

--- chat-app/src/components/LeftSideBar/LeftSideBar.jsx
+++ chat-app/src/components/LeftSideBar/LeftSideBar.jsx
@@ -18,13 +18,15 @@ const LeftSideBar = () => {
                 </div>
             </div>
             <div className="ls-list">
-                <div className="friends">
+                {Array(12).fill("").map((item, index)=>(
+                 <div key={index} className="friends">
                     <img src={assets.profile_img} alt="" />
                     <div>
                         <p>Richard Standford</p>
                         <span>Hello, How are you?</span>
                     </div>
                 </div>
+                ))}
             </div>
         </div>
     )
