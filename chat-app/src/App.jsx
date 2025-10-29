4e27bf30eb16f131d0a5bdd2c82c3a075ecbec1c
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-10-29 10:49:47 +0100

    worked on the profile update page

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Login/>} />
-        <Route path="/profile-update" element={<ProfileUpdate/>} />
+        <Route path="/profile" element={<ProfileUpdate/>} />
         <Route path="/chat" element={<Chat/>} />
       </Routes>
     </>
