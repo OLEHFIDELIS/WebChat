0ddf80a7262e1028194e90a65c8ca72bc7192388
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-11-26 21:48:40 +0100

    workord the chat app

---
+++ chat-app/src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmrN8JxlzKN5Wp_MAaVNDuFEwlz-n-GUY",
+  authDomain: "chap-app-gs-27062.firebaseapp.com",
+  projectId: "chap-app-gs-27062",
+  storageBucket: "chap-app-gs-27062.firebasestorage.app",
+  messagingSenderId: "186640684448",
+  appId: "1:186640684448:web:ad46bd9d270e8e709740c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
